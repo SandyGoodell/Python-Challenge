19bdf88ed8a55b277c66879a9d8abd8bf68b4030
Sandy Goodell <sgoodel3@uoregon.edu>
2021-01-11 10:47:51 -0800

    2nd for Loop

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,19 +3,64 @@ import os
 import csv
 
 # import data (path)
-file_to_open = os.path.join()
+file_to_open = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join("results","election_results.txt")
 
 # declare global variables
+# Total # of votes cast
+total_votes = 0
+
 
 # if using functions create 
 
 # start loop - with open
 with open(file_to_open, "r") as csvfile:
-    csvreader = csv.reader(csvfile)
+    csvreader = csv.reader(csvfile, delimiter=',')
 
     # This skips the first row of the CSV file.
     next(csvreader)
 
+    # List of candidates
+    candidates = []
+
+    # dictionary
+    candidate_votes = {}
     for row in csvreader:
         # do stuff with rows...
+        # total number of votes cast 
+        total_votes = total_votes + 1
+
+        # List of candidates that received votes
+        candidate_name = row[2]
+        if candidate_name not in candidates:
+            candidates.append(candidate_name)
+            # Total No of votes and % of Total votes
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+    # open file to print up here, you will need to tab everything in
+    print(f"Election Results \n"
+    f"-------------------------\n"
+    f"Total Votes: {total_votes}\n"
+    f"-------------------------\n")
+    # this needs to be while we are are in the candidates and do we need to sort the data by candidate?
+    # percentage is = to candidate_votes / total_votes 
+    # percentage_votes = total_votes / candidate_votes
+    print(candidate_votes)
+    for candidate in candidates:
+        votes = candidate_votes.get(candidate)
+        percentage_votes = (candidate_votes.get(candidate)/total_votes) * 100
+        print(percentage_votes)
+
+        print(votes)
+
+
+
+    # Print to txt file in Analysis folder
+    
+    with open(output_path, 'w') as txtfile:
+        txtfile.write(f"text\n")
+
+
+
+
 
